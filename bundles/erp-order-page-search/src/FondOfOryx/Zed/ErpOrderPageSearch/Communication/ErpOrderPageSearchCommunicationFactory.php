c51c5891bfa15f9f04c5fc4c3fc223fa3710b344
Jozsef Geng <gengjozsef86@gmail.com>
2021-02-02 16:08:31 +0100

    add annotations for ErpOrderPageSearchCommunicationFactory

--- bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/Communication/ErpOrderPageSearchCommunicationFactory.php
+++ bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/Communication/ErpOrderPageSearchCommunicationFactory.php
@@ -3,6 +3,11 @@ namespace FondOfOryx\Zed\ErpOrderPageSearch\Communication;
 
 use Spryker\Zed\Kernel\Communication\AbstractCommunicationFactory;
 
+/**
+ * @method \FondOfOryx\Zed\ErpOrderPageSearch\Persistence\ErpOrderPageSearchQueryContainerInterface getQueryContainer()
+ * @method \FondOfOryx\Zed\ErpOrderPageSearch\ErpOrderPageSearchConfig getConfig()
+ * @method \FondOfOryx\Zed\ErpOrderPageSearch\Business\ErpOrderPageSearchFacadeInterface getFacade()
+ */
 class ErpOrderPageSearchCommunicationFactory extends AbstractCommunicationFactory
 {
 
