20bd039ed27f3431091409ed35fe704fa9ddd4e2
Jozsef Geng <gengjozsef86@gmail.com>
2021-02-02 14:09:35 +0100

    add Facade publish and unpublish methods

--- bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/Business/ErpOrderPageSearchFacade.php
+++ bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/Business/ErpOrderPageSearchFacade.php
@@ -1,9 +1,39 @@
 <?php
+
 namespace FondOfOryx\Zed\ErpOrderPageSearch\Business;
 
 use Spryker\Zed\Kernel\Business\AbstractFacade;
 
 class ErpOrderPageSearchFacade extends AbstractFacade
 {
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @param array $erpOrderIds
+     *
+     * @return void
+     */
+    public function publish(array $erpOrderIds): void
+    {
+        $this->getFactory()->createErpOrderPageSearchPublisher()
+            ->publish($erpOrderIds);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @param array $erpOrderIds
+     *
+     * @return void
+     */
+    public function unpublish(array $erpOrderIds): void
+    {
+        $this->getFactory()->createErpOrderPageSearchUnPublisher()
+            ->unpublish($erpOrderIds);
+    }
 
 }
