bd8c1df9d7ee2238085658e6ab229242d4815254
Jozsef Geng <jozsef.geng@fondof.de>
2021-02-02 09:35:42 +0100

    add Classes for the initial skeleton

---
+++ bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/ErpOrderPageSearchDependencyProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FondOfOryx\Zed\ErpOrderPageSearch;
+
+/**
+ * @method \FondOfOryx\Zed\ErporderPageSearch\ErpOrderPageSearchConfig getConfig()
+ */
+class ErpOrderPageSearchDependencyProvider extends AbstractBundleDependencyProvider
+{
+
+}
