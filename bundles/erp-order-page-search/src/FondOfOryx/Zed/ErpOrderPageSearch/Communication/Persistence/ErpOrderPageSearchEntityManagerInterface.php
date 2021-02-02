d72b5142c0dca15efb05198bf53a975efc7c4d7d
Jozsef Geng <gengjozsef86@gmail.com>
2021-02-02 14:07:44 +0100

    add Create/Delete Search Entries

--- bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/Communication/Persistence/ErpOrderPageSearchEntityManagerInterface.php
+++ bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/Communication/Persistence/ErpOrderPageSearchEntityManagerInterface.php
@@ -4,5 +4,15 @@ namespace FondOfOryx\Zed\ErpOrderPageSearch\Persistence;
 
 interface ErpOrderPageSearchEntityManagerInterface
 {
-   
+    /**
+     * @param array $erpOrderIds
+     *
+     * @return void
+     */
+    public function deleteErpOrderSearchPagesByErpOrderIds(array $erpOrderIds): void;
+
+    /**
+     * @param \FondOfOryx\Zed\ErpOrderPageSearch\Persistence\ErpOrderPageSearchTransfer $erpOrderPageSearchTransfer
+     */
+    public function createErpOrderPageSearch(ErpOrderPageSearchTransfer $erpOrderPageSearchTransfer): void;
 }
