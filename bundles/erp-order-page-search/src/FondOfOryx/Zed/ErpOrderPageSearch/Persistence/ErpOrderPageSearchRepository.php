f94440f87b2239427856d5f48b3dadf6e0c5f141
Julian Zimmermann <julian.zimmermann@fondof.de>
2021-02-02 16:58:46 +0100

    - fix

--- bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/Persistence/ErpOrderPageSearchRepository.php
+++ bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/Persistence/ErpOrderPageSearchRepository.php
@@ -4,7 +4,10 @@ namespace FondOfOryx\Zed\ErpOrderPageSearch\Persistence;
 
 use Spryker\Zed\Kernel\Persistence\AbstractRepository;
 
+/**
+ * @method \FondOfOryx\Zed\ErpOrderPageSearch\Persistence\ErpOrderPageSearchPersistenceFactory getFactory()
+ */
 class ErpOrderPageSearchRepository extends AbstractRepository implements ErpOrderPageSearchRepositoryInterface
 {
-   
+
 }
