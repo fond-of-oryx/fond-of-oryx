1131d0659cee2372c406bd0811c1c539950c894f
Jozsef Geng <gengjozsef86@gmail.com>
2021-02-03 08:05:37 +0100

    Dependency Provider add Erp Order Page Search Query

--- bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/ErpOrderPageSearchDependencyProvider.php
+++ bundles/erp-order-page-search/src/FondOfOryx/Zed/ErpOrderPageSearch/ErpOrderPageSearchDependencyProvider.php
@@ -3,6 +3,7 @@
 namespace FondOfOryx\Zed\ErpOrderPageSearch;
 
 use FondOfOryx\Zed\ErporderPageSearch\Dependency\Service\ErpOrderPageSearchToUtilEncodingServiceBridge;
+use Orm\Zed\ErpOrderPageSearch\Persistence\FooErpOrderPageSearchQuery;
 use Spryker\Zed\Kernel\Container;
 
 /**
@@ -11,6 +12,7 @@ use Spryker\Zed\Kernel\Container;
 class ErpOrderPageSearchDependencyProvider extends AbstractBundleDependencyProvider
 {
     public const SERVICE_UTIL_ENCODING = 'SERVICE_UTIL_ENCODING';
+    public const QUERY_ERP_ORDER_PAGE_SEARCH = 'QUERY_ERP_ORDER_PAGE_SEARCH';
 
     /**
      * @param \Spryker\Zed\Kernel\Container $container
@@ -31,12 +33,40 @@ class ErpOrderPageSearchDependencyProvider extends AbstractBundleDependencyProvi
      *
      * @return \Spryker\Zed\Kernel\Container
      */
+    public function providePersistenceLayerDependencies(Container $container): Container
+    {
+        $container = parent::providePersistenceLayerDependencies($container);
+
+        $container = $this->addErpOrderPageSearchQuery($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
     protected function addUtilEncodingService(Container $container): Container
     {
         $container[static::SERVICE_UTIL_ENCODING] = static function (Container $container) {
             return new ErpOrderPageSearchToUtilEncodingServiceBridge(
                 $container->getLocator()->utilEncoding()->service()
             );
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addErpOrderPageSearchQuery(Container $container): Container
+    {
+        $container[static::QUERY_ERP_ORDER_PAGE_SEARCH] = static function () {
+            return FooErpOrderPageSearchQuery::create();
         };
 
         return $container;
