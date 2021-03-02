3e6be0daf0668127772b58b1c9d747fcdc976dfc
Daniel Kemna <danielkemna@t-online.de>
2021-03-02 15:33:18 +0100

    Fix error code from int to string

--- bundles/one-time-password-rest-api/src/FondOfOryx/Glue/OneTimePasswordRestApi/OneTimePasswordRestApiConfig.php
+++ bundles/one-time-password-rest-api/src/FondOfOryx/Glue/OneTimePasswordRestApi/OneTimePasswordRestApiConfig.php
@@ -9,6 +9,6 @@ class OneTimePasswordRestApiConfig extends AbstractBundleConfig
     public const RESOURCE_ONE_TIME_PASSWORD = 'one-time-password';
     public const CONTROLLER_ONE_TIME_PASSWORD = 'one-time-password-resource';
 
-    public const EMAIL_REQUIRED_ERROR_CODE = 1000;
+    public const EMAIL_REQUIRED_ERROR_CODE = '1000';
     public const EMAIL_REQUIRED_ERROR_DETAIL = 'The attribute Email is required.';
 }
