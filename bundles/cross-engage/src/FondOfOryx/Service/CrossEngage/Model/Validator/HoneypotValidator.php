05b796175f922560501246a399cdbc96ae355ea9
Julian Zimmermann <54834203+julianzimmermann@users.noreply.github.com>
2021-03-02 16:45:55 +0100

    Feature/cross engange (#14)
    - extracted some CrossEngage stuff and moved it to a service
    - added tests dir
    - fix composer dependencies
    - fix readme

---
+++ bundles/cross-engage/src/FondOfOryx/Service/CrossEngage/Model/Validator/HoneypotValidator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FondOfOryx\Service\CrossEngage\Model\Validator;
+
+use FondOfOryx\Service\CrossEngage\Exception\FormValidatorValidationErrorException;
+use Symfony\Component\Form\FormInterface;
+
+class HoneypotValidator implements FormValidatorInterface
+{
+    public const NAME = 'HoneypotValidator';
+
+    /**
+     * @param \Symfony\Component\Form\FormInterface $form
+     *
+     * @throws \FondOfOryx\Service\CrossEngage\Exception\FormValidatorValidationErrorException
+     *
+     * @return bool
+     */
+    public function validate(FormInterface $form): bool
+    {
+        if ($form->get('name')->getData() !== null) {
+            throw new FormValidatorValidationErrorException(sprintf('%s failt to validate!', $this->getName()));
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return static::NAME;
+    }
+}
