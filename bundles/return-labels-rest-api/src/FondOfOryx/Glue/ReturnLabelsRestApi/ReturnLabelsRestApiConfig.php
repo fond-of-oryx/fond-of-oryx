f225158e140fcb79e0195545da76b291d9c33892
Pascal Fischer <p-fischer@gmx.net>
2021-03-24 08:32:31 +0100

    resources are named by plural

--- bundles/return-labels-rest-api/src/FondOfOryx/Glue/ReturnLabelsRestApi/ReturnLabelsRestApiConfig.php
+++ bundles/return-labels-rest-api/src/FondOfOryx/Glue/ReturnLabelsRestApi/ReturnLabelsRestApiConfig.php
@@ -7,6 +7,6 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 class ReturnLabelsRestApiConfig extends AbstractBundleConfig
 {
     public const ACTION_RETURN_LABEL_REST_API_POST = 'post';
-    public const RESOURCE_RETURN_LABEL_REST_API = 'return-label-rest-api';
-    public const CONTROLLER_RETURN_LABEL_REST_API = 'return-label-rest-api-resource';
+    public const RESOURCE_RETURN_LABEL_REST_API = 'return-labels-rest-api';
+    public const CONTROLLER_RETURN_LABEL_REST_API = 'return-labels-rest-api-resource';
 }
