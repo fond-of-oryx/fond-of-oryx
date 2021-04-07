93ff2912c4b548f45303c123596ea99cb97db6cc
Daniel Rose <daniel-rose@gmx.de>
2021-04-07 06:32:23 +0200

    Implement contracts for product country restriction module 📜

---
+++ bundles/product-country-restriction/src/FondOfOryx/Zed/ProductCountryRestriction/Persistence/Propel/Mapper/ProductAbstractCountryRestrictionMapper.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace FondOfOryx\Zed\ProductCountryRestriction\Persistence\Propel\Mapper;
+
+use Generated\Shared\Transfer\CountryTransfer;
+use Generated\Shared\Transfer\ProductAbstractCountryRestrictionTransfer;
+use Orm\Zed\ProductCountryRestriction\Persistence\FooProductAbstractCountryRestriction;
+use Propel\Runtime\Collection\ObjectCollection;
+
+class ProductAbstractCountryRestrictionMapper implements ProductAbstractCountryRestrictionMapperInterface
+{
+    /**
+     * @param \Propel\Runtime\Collection\ObjectCollection $fooProductAbstractCountryRestrictionCollection
+     *
+     * @return \Generated\Shared\Transfer\CountryTransfer[]
+     */
+    public function mapEntityCollectionToCountryTransfers(
+        ObjectCollection $fooProductAbstractCountryRestrictionCollection
+    ): array {
+        $countryTransfers = [];
+
+        foreach ($fooProductAbstractCountryRestrictionCollection as $fooProductAbstractCountryRestriction) {
+            $spyCountry = $fooProductAbstractCountryRestriction->getCountry();
+            $countryTransfers[] = (new CountryTransfer())->fromArray($spyCountry->toArray());
+        }
+
+        return $countryTransfers;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductAbstractCountryRestrictionTransfer $transfer
+     * @param \Orm\Zed\ProductCountryRestriction\Persistence\FooProductAbstractCountryRestriction $entity
+     *
+     * @return \Orm\Zed\ProductCountryRestriction\Persistence\FooProductAbstractCountryRestriction
+     */
+    public function mapTransferToEntity(
+        ProductAbstractCountryRestrictionTransfer $transfer,
+        FooProductAbstractCountryRestriction $entity
+    ): FooProductAbstractCountryRestriction {
+        $entity->fromArray($transfer->toArray());
+
+        return $entity->setFkProductAbstract($transfer->getIdCountry())
+            ->setFkProductAbstract($transfer->getIdProductAbstract());
+    }
+
+    /**
+     * @param \Propel\Runtime\Collection\ObjectCollection|\Orm\Zed\ProductCountryRestriction\Persistence\FooProductAbstractCountryRestriction[] $fooProductAbstractCountryRestrictionCollection
+     * @param string|null $virtualColumnForGrouping
+     *
+     * @return array
+     */
+    public function mapEntityCollectionToGroupedCountryNames(
+        ObjectCollection $fooProductAbstractCountryRestrictionCollection,
+        ?string $virtualColumnForGrouping = null
+    ): array {
+        $groupedCountryIds = [];
+
+        foreach ($fooProductAbstractCountryRestrictionCollection as $fooProductAbstractCountryRestriction) {
+            $key = $fooProductAbstractCountryRestriction->getFkProductAbstract();
+            $country = $fooProductAbstractCountryRestriction->getCountry();
+
+            if ($virtualColumnForGrouping !== null) {
+                $key = $fooProductAbstractCountryRestriction->getVirtualColumn($virtualColumnForGrouping);
+            }
+
+            if (!isset($groupedCountryIds[$key])) {
+                $groupedCountryIds[$key] = [];
+            }
+
+            $groupedCountryIds[$key][] = $country->getIso2Code();
+        }
+
+        return $groupedCountryIds;
+    }
+}
---
+++ bundles/product-country-restriction/tests/FondOfOryx/Zed/ProductCountryRestriction/Communication/Plugin/Product/ProductAbstractCountryRestrictionProductAbstractAfterUpdatePluginTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace FondOfOryx\Zed\ProductCountryRestriction\Communication\Plugin\Product;
+
+use Codeception\Test\Unit;
+use FondOfOryx\Zed\ProductCountryRestriction\Business\ProductCountryRestrictionFacade;
+use Generated\Shared\Transfer\ProductAbstractTransfer;
+
+class ProductAbstractCountryRestrictionProductAbstractAfterUpdatePluginTest extends Unit
+{
+    /**
+     * @var \FondOfOryx\Zed\ProductCountryRestriction\Business\ProductCountryRestrictionFacade|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $facadeMock;
+
+    /**
+     * @var \Generated\Shared\Transfer\ProductAbstractTransfer|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $productAbstractTransferMock;
+
+    /**
+     * @var \FondOfOryx\Zed\ProductCountryRestriction\Communication\Plugin\Product\ProductAbstractCountryRestrictionProductAbstractAfterUpdatePlugin
+     */
+    protected $plugin;
+
+    /**
+     * @return void
+     */
+    protected function _before(): void
+    {
+        parent::_before();
+
+        $this->facadeMock = $this->getMockBuilder(ProductCountryRestrictionFacade::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->productAbstractTransferMock = $this->getMockBuilder(ProductAbstractTransfer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->plugin = new ProductAbstractCountryRestrictionProductAbstractAfterUpdatePlugin();
+        $this->plugin->setFacade($this->facadeMock);
+    }
+
+    /**
+     * @return void
+     */
+    public function testUpdate(): void
+    {
+        $this->facadeMock->expects(static::atLeastOnce())
+            ->method('persistProductAbstractCountryRestrictions')
+            ->with($this->productAbstractTransferMock);
+
+        static::assertEquals(
+            $this->productAbstractTransferMock,
+            $this->plugin->update($this->productAbstractTransferMock)
+        );
+    }
+}
