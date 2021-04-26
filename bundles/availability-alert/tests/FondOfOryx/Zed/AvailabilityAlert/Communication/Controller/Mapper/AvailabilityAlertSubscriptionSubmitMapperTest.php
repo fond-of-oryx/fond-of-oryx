8a31e9067a8aae801a4c143beafb7399698a9d78
Daniel Rose <daniel-rose@gmx.de>
2021-04-26 09:22:00 +0200

    Fix unit test

--- bundles/availability-alert/tests/FondOfOryx/Zed/AvailabilityAlert/Communication/Controller/Mapper/AvailabilityAlertSubscriptionSubmitMapperTest.php
+++ bundles/availability-alert/tests/FondOfOryx/Zed/AvailabilityAlert/Communication/Controller/Mapper/AvailabilityAlertSubscriptionSubmitMapperTest.php
@@ -81,7 +81,7 @@ class AvailabilityAlertSubscriptionSubmitMapperTest extends Unit
         $this->subscriptionRequestTransferMock->expects(static::once())->method('modifiedToArray')->willReturn([]);
         $this->subscriptionRequestTransferMock->expects(static::once())->method('getIdProductAbstract')->willReturn(1);
         $this->subscriptionRequestTransferMock->expects(static::once())->method('getLocaleName')->willReturn('test');
-        $this->subscriptionRequestTransferMock->expects(static::once())->method('getStore')->willReturn('test');
+        $this->subscriptionRequestTransferMock->expects(static::once())->method('getStoreByName')->willReturn('test');
         $this->subscriptionRequestTransferMock->expects(static::once())->method('toArray')->willReturn([]);
         $this->localeFacadeMock->expects(static::once())->method('getLocale')->willReturn($this->localeTransferMock);
         $this->localeTransferMock->expects(static::once())->method('getIdLocale')->willReturn(1);
