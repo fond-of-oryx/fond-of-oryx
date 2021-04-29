9489ff7b69f6f4680924197a04d442eafdaa4508
Pascal Fischer <p-fischer@gmx.net>
2021-04-29 15:09:29 +0200

    api doc block

--- bundles/return-labels-rest-api/src/FondOfOryx/Glue/ReturnLabelsRestApi/Controller/ReturnLabelsRestApiResourceController.php
+++ bundles/return-labels-rest-api/src/FondOfOryx/Glue/ReturnLabelsRestApi/Controller/ReturnLabelsRestApiResourceController.php
@@ -13,6 +13,22 @@ use Spryker\Glue\Kernel\Controller\AbstractController;
 class ReturnLabelsRestApiResourceController extends AbstractController
 {
     /**
+     * @Glue({
+     *     "getResourceById": {
+     *          "path": "/return-labels/{company-unit-address-uuid}",
+     *          "summary": [
+     *              "Retrieves a company business unit address by customer and company-unit-address-uuid"
+     *          ],
+     *          "parameters": [{
+     *              "ref": "acceptLanguage"
+     *          }],
+     *          "responses": {
+     *              "404": "Address for customer not found.",
+     *.             "422": "company-unit-address country not allowed for return-label"
+     *          }
+     *     }
+     * })
+     *
      * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
      *
      * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface
