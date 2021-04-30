b6fa1fe57abf3516cbd8f2a1b8a70fc3c1fd5b44
DerCalli <35733274+DerCalli@users.noreply.github.com>
2021-04-30 08:58:06 +0200

    return-labels-rest-api: glue layer + unit tests (#99)
    * glue layer + unit tests
    
    * api doc block
    
    * return correct http status code on error

---
+++ bundles/return-labels-rest-api/src/FondOfOryx/Glue/ReturnLabelsRestApi/Controller/ReturnLabelsRestApiResourceController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace FondOfOryx\Glue\ReturnLabelsRestApi\Controller;
+
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface;
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+use Spryker\Glue\Kernel\Controller\AbstractController;
+
+/**
+ * @method \FondOfOryx\Glue\ReturnLabelsRestApi\ReturnLabelsRestApiFactory getFactory()
+ */
+class ReturnLabelsRestApiResourceController extends AbstractController
+{
+    /**
+     * @Glue({
+     *     "getResourceById": {
+     *          "path": "/return-labels/{company-unit-address-uuid}",
+     *          "summary": [
+     *              "Retrieves a company business unit address by customer and company-unit-address-uuid"
+     *          ],
+     *          "parameters": [{
+     *              "ref": "acceptLanguage"
+     *          }],
+     *          "responses": {
+     *              "404": "Address for customer not found.",
+     *.             "422": "Could not process requested country"
+     *          }
+     *     }
+     * })
+     *
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface
+     */
+    public function getAction(RestRequestInterface $restRequest): RestResponseInterface
+    {
+        return $this->getFactory()
+            ->createReturnLabelProcessor()
+            ->getReturnLabel($restRequest);
+    }
+}
---
+++ bundles/return-labels-rest-api/tests/FondOfOryx/Glue/ReturnLabelsRestApi/Plugin/GlueApplicationExtension/ReturnLabelsRestApiResourceRoutePluginTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace FondOfOryx\Glue\ReturnLabelsRestApi\Plugin\GlueApplicationExtension;
+
+use Codeception\Test\Unit;
+use FondOfOryx\Glue\ReturnLabelsRestApi\ReturnLabelsRestApiConfig;
+use Generated\Shared\Transfer\RestReturnLabelRequestAttributesTransfer;
+use Spryker\Glue\GlueApplication\Rest\Collection\ResourceRouteCollection;
+use Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRouteCollectionInterface;
+
+class ReturnLabelsRestApiResourceRoutePluginTest extends Unit
+{
+    /**
+     * @var \FondOfOryx\Glue\ReturnLabelsRestApi\Plugin\GlueApplicationExtension\ReturnLabelsRestApiResourceRoutePlugin
+     */
+    protected $plugin;
+
+    /**
+     * @var \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRouteCollectionInterface|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $resourceRouteCollectionMock;
+
+    /**
+     * @return void
+     */
+    protected function _before(): void
+    {
+        parent::_before();
+
+        $this->resourceRouteCollectionMock = $this
+            ->getMockBuilder(ResourceRouteCollection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->plugin = new ReturnLabelsRestApiResourceRoutePlugin();
+    }
+
+    /**
+     * @return void
+     */
+    public function testConfigure(): void
+    {
+        $this->resourceRouteCollectionMock->expects($this->once())
+            ->method('addGet')
+            ->willReturn($this->resourceRouteCollectionMock);
+
+        $collection = $this->plugin->configure($this->resourceRouteCollectionMock);
+
+        $this->assertInstanceOf(ResourceRouteCollectionInterface::class, $collection);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetResourceType(): void
+    {
+        $this->assertSame(ReturnLabelsRestApiConfig::RESOURCE_RETURN_LABELS_REST_API, $this->plugin->getResourceType());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetController(): void
+    {
+        $this->assertSame(ReturnLabelsRestApiConfig::CONTROLLER_RETURN_LABELS_REST_API, $this->plugin->getController());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetResourceAttributesClassName(): void
+    {
+        $this->assertSame(RestReturnLabelRequestAttributesTransfer::class, $this->plugin->getResourceAttributesClassName());
+    }
+}
