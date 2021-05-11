de9044d4964b6787de6535003fa60fa5ae3856eb
Julian Zimmermann <julian.zimmermann@fondof.de>
2021-05-11 12:05:45 +0200

    - add option to override the default activation state via config

--- bundles/availability-alert-cross-engage/src/FondOfOryx/Shared/AvailabilityAlertCrossEngage/AvailabilityAlertCrossEngageConstants.php
+++ bundles/availability-alert-cross-engage/src/FondOfOryx/Shared/AvailabilityAlertCrossEngage/AvailabilityAlertCrossEngageConstants.php
@@ -5,4 +5,5 @@ namespace FondOfOryx\Shared\AvailabilityAlertCrossEngage;
 interface AvailabilityAlertCrossEngageConstants
 {
     public const AVAILABILITY_ALERT_CROSS_ENGAGE_SALT = 'AVAILABILITY_ALERT_CROSS_ENGAGE:SALT';
+    public const DEFAULT_SUBSCRIBER_ACTIVATION_STATE = 'DEFAULT_SUBSCRIBER_ACTIVATION_STATE';
 }
