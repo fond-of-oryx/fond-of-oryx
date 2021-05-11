0c51eb3adfb74912c9a8681c50e73e7a9346fd5e
Daniel Rose <daniel-rose@gmx.de>
2021-05-11 16:34:02 +0200

    Add query expander for (concrete) sku pattern (#126)
    - allow to search after (concrete) sku with wildcard pattern
    - add plugins to expand query by new request parameters (concrete-sku-pattern & sku-pattern)
    - use php >=7.3 (lower versions are eol)
    - ...

--- bundles/catalog-sku-filter/src/FondOfOryx/Shared/CatalogSkuFilter/CatalogSkuFilterConstants.php
+++ bundles/catalog-sku-filter/src/FondOfOryx/Shared/CatalogSkuFilter/CatalogSkuFilterConstants.php
@@ -5,5 +5,8 @@ namespace FondOfOryx\Shared\CatalogSkuFilter;
 interface CatalogSkuFilterConstants
 {
     public const PARAMETER_SKU = 'sku';
+    public const PARAMETER_SKU_PATTERN = 'sku-pattern';
+
     public const PARAMETER_CONCRETE_SKU = 'concrete-sku';
+    public const PARAMETER_CONCRETE_SKU_PATTERN = 'concrete-sku-pattern';
 }
