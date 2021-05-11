40c40a1fa104e0aef232af787dfb6e2056ecb244
Daniel Rose <daniel-rose@gmx.de>
2021-05-11 16:34:02 +0200

    Add query expander for (concrete) sku pattern
    - allow to search after (concrete) sku with wildcard pattern
    - add plugins to expand query by new request parameters (concrete-sku-pattern & sku-pattern)
    - use php >=7.3 (lower versions are eol)
    - ...

---
+++ bundles/catalog-sku-filter/src/FondOfOryx/Client/CatalogSkuFilter/Plugin/SearchExtension/ConcreteSkuPatternQueryExpanderPlugin.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace FondOfOryx\Client\CatalogSkuFilter\Plugin\SearchExtension;
+
+use Elastica\Query;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\Wildcard;
+use FondOfOryx\Shared\CatalogSkuFilter\CatalogSkuFilterConstants;
+use Generated\Shared\Search\PageIndexMap;
+use InvalidArgumentException;
+use Spryker\Client\SearchExtension\Dependency\Plugin\QueryExpanderPluginInterface;
+use Spryker\Client\SearchExtension\Dependency\Plugin\QueryInterface;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+class ConcreteSkuPatternQueryExpanderPlugin extends AbstractPlugin implements QueryExpanderPluginInterface
+{
+    /**
+     * @param \Spryker\Client\SearchExtension\Dependency\Plugin\QueryInterface $searchQuery
+     * @param array $requestParameters
+     *
+     * @return \Spryker\Client\SearchExtension\Dependency\Plugin\QueryInterface
+     */
+    public function expandQuery(
+        QueryInterface $searchQuery,
+        array $requestParameters = []
+    ): QueryInterface {
+        if (!isset($requestParameters[CatalogSkuFilterConstants::PARAMETER_CONCRETE_SKU_PATTERN])) {
+            return $searchQuery;
+        }
+
+        $concreteSkuPattern = $requestParameters[CatalogSkuFilterConstants::PARAMETER_CONCRETE_SKU_PATTERN];
+
+        if (!is_string($concreteSkuPattern) || $concreteSkuPattern === '') {
+            return $searchQuery;
+        }
+
+        $this->getBoolQuery($searchQuery->getSearchQuery())
+            ->addMust(new Wildcard(PageIndexMap::CONCRETE_SKUS, $concreteSkuPattern));
+
+        return $searchQuery;
+    }
+
+    /**
+     * @param \Elastica\Query $query
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return \Elastica\Query\BoolQuery
+     */
+    protected function getBoolQuery(Query $query): BoolQuery
+    {
+        $boolQuery = $query->getQuery();
+
+        if (!$boolQuery instanceof BoolQuery) {
+            throw new InvalidArgumentException(sprintf(
+                'Sku pattern query expander available only with %s, got: %s',
+                BoolQuery::class,
+                get_class($boolQuery)
+            ));
+        }
+
+        return $boolQuery;
+    }
+}
