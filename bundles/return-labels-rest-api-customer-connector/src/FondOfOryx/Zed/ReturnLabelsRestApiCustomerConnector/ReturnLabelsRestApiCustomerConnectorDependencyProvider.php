d679a124a582a1dfb32fb457f135ce78a6f7e5a8
Pascal Fischer <p-fischer@gmx.net>
2021-05-12 14:07:14 +0200

    missing unit tests, requested change of PR #124

--- bundles/return-labels-rest-api-customer-connector/src/FondOfOryx/Zed/ReturnLabelsRestApiCustomerConnector/ReturnLabelsRestApiCustomerConnectorDependencyProvider.php
+++ bundles/return-labels-rest-api-customer-connector/src/FondOfOryx/Zed/ReturnLabelsRestApiCustomerConnector/ReturnLabelsRestApiCustomerConnectorDependencyProvider.php
@@ -3,28 +3,12 @@
 namespace FondOfOryx\Zed\ReturnLabelsRestApiCustomerConnector;
 
 use FondOfOryx\Zed\ReturnLabelsRestApiCustomerConnector\Dependency\Facade\ReturnLabelsRestApiCustomerConnectorToCustomerFacadeBridge;
-use Orm\Zed\Customer\Persistence\SpyCustomerQuery;
 use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
 use Spryker\Zed\Kernel\Container;
 
 class ReturnLabelsRestApiCustomerConnectorDependencyProvider extends AbstractBundleDependencyProvider
 {
-    public const PROPEL_QUERY_CUSTOMER_QUERY = 'PROPEL_QUERY_CUSTOMER_QUERY';
-
     public const FACADE_CUSTOMER = 'FACADE_CUSTOMER';
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
-    public function providePersistenceLayerDependencies(Container $container): Container
-    {
-        $container = parent::providePersistenceLayerDependencies($container);
-        $container = $this->addCustomerQuery($container);
-
-        return $container;
-    }
 
     /**
      * @param \Spryker\Zed\Kernel\Container $container
@@ -35,20 +19,6 @@ class ReturnLabelsRestApiCustomerConnectorDependencyProvider extends AbstractBun
     {
         $container = parent::provideBusinessLayerDependencies($container);
         $container = $this->addCustomerFacade($container);
-
-        return $container;
-    }
-
-    /**
-     * @param \Spryker\Zed\Kernel\Container $container
-     *
-     * @return \Spryker\Zed\Kernel\Container
-     */
-    protected function addCustomerQuery(Container $container): Container
-    {
-        $container[static::PROPEL_QUERY_CUSTOMER_QUERY] = static function () {
-            return SpyCustomerQuery::create();
-        };
 
         return $container;
     }
