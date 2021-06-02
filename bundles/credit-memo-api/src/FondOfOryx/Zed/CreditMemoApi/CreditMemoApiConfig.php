4458ddfdff40f9ec1a1c83f9f2245420eae302fc
Julian Zimmermann <julian.zimmermann@fondof.de>
2021-06-02 11:27:10 +0200

    move CreditMemoApi package from fos to foo

---
+++ bundles/credit-memo-api/src/FondOfOryx/Zed/CreditMemoApi/CreditMemoApiConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FondOfOryx\Zed\CreditMemoApi;
+
+use Spryker\Zed\Kernel\AbstractBundleConfig;
+
+class CreditMemoApiConfig extends AbstractBundleConfig
+{
+    public const RESOURCE_CREDIT_MEMO = 'credit-memos';
+}
