0bc6a2d613ba376f02805371d2204a690b95fecf
Julian Zimmermann <julian.zimmermann@fondof.de>
2021-06-02 11:09:43 +0200

    moved PrepaymentCreditMemo package from fos to foo

---
+++ bundles/prepayment-credit-memo/src/FondOfOryx/Zed/PrepaymentCreditMemo/PrepaymentCreditMemoDependencyProvider.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace FondOfOryx\Zed\PrepaymentCreditMemo;
+
+use FondOfOryx\Zed\PrepaymentCreditMemo\Dependency\Facade\PrepaymentCreditMemoToCreditMemoBridge;
+use FondOfOryx\Zed\PrepaymentCreditMemo\Dependency\Facade\PrepaymentCreditMemoToOmsBridge;
+use FondOfOryx\Zed\PrepaymentCreditMemo\Dependency\Facade\PrepaymentCreditMemoToRefundBridge;
+use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Zed\Kernel\Container;
+
+class PrepaymentCreditMemoDependencyProvider extends AbstractBundleDependencyProvider
+{
+    public const FACADE_REFUND = 'refund facade';
+    public const FACADE_CREDIT_MEMO = 'FACADE_CREDIT_MEMO';
+    public const FACADE_OMS = 'FACADE_OMS';
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideBusinessLayerDependencies(Container $container)
+    {
+        $container = $this->addRefundFacade($container);
+        $container = $this->addCreditMemoFacade($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideCommunicationLayerDependencies(Container $container)
+    {
+        $container = parent::provideCommunicationLayerDependencies($container);
+        $container = $this->addCreditMemoFacade($container);
+        $container = $this->addRefundFacade($container);
+        $container = $this->addOmsFacade($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addRefundFacade(Container $container)
+    {
+        $container[self::FACADE_REFUND] = function (Container $container) {
+            return new PrepaymentCreditMemoToRefundBridge($container->getLocator()->refund()->facade());
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addCreditMemoFacade(Container $container)
+    {
+        $container[self::FACADE_CREDIT_MEMO] = function (Container $container) {
+            return new PrepaymentCreditMemoToCreditMemoBridge($container->getLocator()->creditMemo()->facade());
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addOmsFacade(Container $container)
+    {
+        $container[self::FACADE_OMS] = function (Container $container) {
+            return new PrepaymentCreditMemoToOmsBridge($container->getLocator()->oms()->facade());
+        };
+
+        return $container;
+    }
+}
