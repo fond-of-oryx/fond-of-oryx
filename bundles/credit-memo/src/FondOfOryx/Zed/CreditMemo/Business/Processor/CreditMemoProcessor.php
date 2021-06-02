a6ff3ece9d74cc786741d3fbff872fef80994427
Julian Zimmermann <julian.zimmermann@fondof.de>
2021-06-02 15:40:54 +0200

    - phpcs fixes

--- bundles/credit-memo/src/FondOfOryx/Zed/CreditMemo/Business/Processor/CreditMemoProcessor.php
+++ bundles/credit-memo/src/FondOfOryx/Zed/CreditMemo/Business/Processor/CreditMemoProcessor.php
@@ -236,8 +236,8 @@ class CreditMemoProcessor implements Countable, IteratorAggregate, CreditMemoPro
     }
 
     /**
-     * @param  \Generated\Shared\Transfer\CreditMemoTransfer  $creditMemoTransfer
-     * @param  \Throwable  $exception
+     * @param \Generated\Shared\Transfer\CreditMemoTransfer $creditMemoTransfer
+     * @param \Throwable $exception
      *
      * @return \Generated\Shared\Transfer\CreditMemoProcessorStatusTransfer
      */
