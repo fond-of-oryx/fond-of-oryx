fcc3a9480933190950a551903180d1d4f202d89f
Julian Zimmermann <54834203+julianzimmermann@users.noreply.github.com>
2021-06-04 16:06:24 +0200

    Feature/move/payone credit memo (#161)
    moved PayoneCreditMemo package from fos to foo

---
+++ bundles/payone-credit-memo/src/FondOfOryx/Zed/PayoneCreditMemo/Business/PayoneCreditMemoBusinessFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FondOfOryx\Zed\PayoneCreditMemo\Business;
+
+use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
+
+/**
+ * @method \FondOfOryx\Zed\PayoneCreditMemo\PayoneCreditMemoConfig getConfig()
+ */
+class PayoneCreditMemoBusinessFactory extends AbstractBusinessFactory
+{
+}
