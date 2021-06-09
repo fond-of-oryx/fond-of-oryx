b22428f2fda4da2845ad9df904c3de191cc1dba0
Daniel Rose <daniel-rose@gmx.de>
2021-06-09 18:10:58 +0200

    Add splittable checkout rest api customer connector module 🧩
    - expand quote with customer information
    - provide quote expander plugin
    - add unit tests
    - ...

---
+++ bundles/splittable-checkout-rest-api-customer-connector/tests/FondOfOryx/Zed/SplittableCheckoutRestApiCustomerConnector/Business/SplittableCheckoutRestApiCustomerConnectorBusinessFactoryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace FondOfOryx\Zed\SplittableCheckoutRestApiCustomerConnector\Business;
+
+use Codeception\Test\Unit;
+use FondOfOryx\Zed\SplittableCheckoutRestApiCustomerConnector\Business\Expander\QuoteExpander;
+use FondOfOryx\Zed\SplittableCheckoutRestApiCustomerConnector\Persistence\SplittableCheckoutRestApiCustomerConnectorRepository;
+
+class SplittableCheckoutRestApiCustomerConnectorBusinessFactoryTest extends Unit
+{
+    /**
+     * @var \FondOfOryx\Zed\SplittableCheckoutRestApiCustomerConnector\Persistence\SplittableCheckoutRestApiCustomerConnectorRepository|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $repositoryMock;
+
+    /**
+     * @var \FondOfOryx\Zed\SplittableCheckoutRestApiCustomerConnector\Business\SplittableCheckoutRestApiCustomerConnectorBusinessFactory
+     */
+    protected $businessFactory;
+
+    /**
+     * @return void
+     */
+    protected function _before(): void
+    {
+        parent::_before();
+
+        $this->repositoryMock = $this->getMockBuilder(SplittableCheckoutRestApiCustomerConnectorRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->businessFactory = new SplittableCheckoutRestApiCustomerConnectorBusinessFactory();
+        $this->businessFactory->setRepository($this->repositoryMock);
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreateQuoteExpander(): void
+    {
+        static::assertInstanceOf(
+            QuoteExpander::class,
+            $this->businessFactory->createQuoteExpander()
+        );
+    }
+}
