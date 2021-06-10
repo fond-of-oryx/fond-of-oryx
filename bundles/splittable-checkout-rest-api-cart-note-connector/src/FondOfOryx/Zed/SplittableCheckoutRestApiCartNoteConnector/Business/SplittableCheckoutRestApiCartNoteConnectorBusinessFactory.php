c305036a5d68512b22fb746c8c0a54c3633b2996
Jozsef Geng <gengjozsef86@gmail.com>
2021-06-10 13:12:22 +0200

    phphcs fixes

--- bundles/splittable-checkout-rest-api-cart-note-connector/src/FondOfOryx/Zed/SplittableCheckoutRestApiCartNoteConnector/Business/SplittableCheckoutRestApiCartNoteConnectorBusinessFactory.php
+++ bundles/splittable-checkout-rest-api-cart-note-connector/src/FondOfOryx/Zed/SplittableCheckoutRestApiCartNoteConnector/Business/SplittableCheckoutRestApiCartNoteConnectorBusinessFactory.php
@@ -6,5 +6,4 @@ use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 
 class SplittableCheckoutRestApiCartNoteConnectorBusinessFactory extends AbstractBusinessFactory
 {
-
 }
--- bundles/splittable-checkout-rest-api-cart-note-connector/src/FondOfOryx/Zed/SplittableCheckoutRestApiCartNoteConnector/SplittableCheckoutRestApiCartNoteConnectorDependencyProvider.php
+++ bundles/splittable-checkout-rest-api-cart-note-connector/src/FondOfOryx/Zed/SplittableCheckoutRestApiCartNoteConnector/SplittableCheckoutRestApiCartNoteConnectorDependencyProvider.php
@@ -6,5 +6,4 @@ use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
 
 class SplittableCheckoutRestApiCartNoteConnectorDependencyProvider extends AbstractBundleDependencyProvider
 {
-
 }
