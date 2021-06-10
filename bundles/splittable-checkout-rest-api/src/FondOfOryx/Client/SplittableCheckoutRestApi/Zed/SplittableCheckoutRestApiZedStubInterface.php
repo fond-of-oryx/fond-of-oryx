008ab3eb934cd0e4e478c55c4c913ee8a3880429
Daniel Rose <daniel-rose@gmx.de>
2021-06-10 14:40:56 +0200

    Integrate client layer of spittable totals rest api in spittable checkout rest api - remove duplicate code - refactoring - cleanup - add unit tests - ...

--- bundles/splittable-checkout-rest-api/src/FondOfOryx/Client/SplittableCheckoutRestApi/Zed/SplittableCheckoutRestApiZedStubInterface.php
+++ bundles/splittable-checkout-rest-api/src/FondOfOryx/Client/SplittableCheckoutRestApi/Zed/SplittableCheckoutRestApiZedStubInterface.php
@@ -4,6 +4,8 @@ namespace FondOfOryx\Client\SplittableCheckoutRestApi\Zed;
 
 use Generated\Shared\Transfer\RestSplittableCheckoutRequestTransfer;
 use Generated\Shared\Transfer\RestSplittableCheckoutResponseTransfer;
+use Generated\Shared\Transfer\RestSplittableTotalsRequestTransfer;
+use Generated\Shared\Transfer\RestSplittableTotalsResponseTransfer;
 
 interface SplittableCheckoutRestApiZedStubInterface
 {
@@ -15,4 +17,13 @@ interface SplittableCheckoutRestApiZedStubInterface
     public function placeOrder(
         RestSplittableCheckoutRequestTransfer $restSplittableCheckoutRequestTransfer
     ): RestSplittableCheckoutResponseTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\RestSplittableCheckoutRequestTransfer $restSplittableCheckoutRequestTransfer
+     *
+     * @return \Generated\Shared\Transfer\RestSplittableTotalsResponseTransfer
+     */
+    public function getSplittableTotals(
+        RestSplittableCheckoutRequestTransfer $restSplittableCheckoutRequestTransfer
+    ): RestSplittableTotalsResponseTransfer;
 }
