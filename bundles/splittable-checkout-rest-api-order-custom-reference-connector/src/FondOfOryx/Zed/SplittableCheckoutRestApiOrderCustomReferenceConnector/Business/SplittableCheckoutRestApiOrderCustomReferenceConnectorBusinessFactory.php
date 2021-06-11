8a88717181c9e5ed0a47ba364deadec7e7ba276c
Jozsef Geng <gengjozsef86@gmail.com>
2021-06-11 09:53:12 +0200

    phphstan fix

--- bundles/splittable-checkout-rest-api-order-custom-reference-connector/src/FondOfOryx/Zed/SplittableCheckoutRestApiOrderCustomReferenceConnector/Business/SplittableCheckoutRestApiOrderCustomReferenceConnectorBusinessFactory.php
+++ bundles/splittable-checkout-rest-api-order-custom-reference-connector/src/FondOfOryx/Zed/SplittableCheckoutRestApiOrderCustomReferenceConnector/Business/SplittableCheckoutRestApiOrderCustomReferenceConnectorBusinessFactory.php
@@ -9,7 +9,7 @@ use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 class SplittableCheckoutRestApiOrderCustomReferenceConnectorBusinessFactory extends AbstractBusinessFactory
 {
     /**
-     * @return \FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Business\Expander\QuoteExpanderInterface
+     * @return \FondOfOryx\Zed\SplittableCheckoutRestApiCartNoteConnector\Business\Expander\QuoteExpanderInterface
      */
     public function createQuoteExpander(): QuoteExpanderInterface
     {
