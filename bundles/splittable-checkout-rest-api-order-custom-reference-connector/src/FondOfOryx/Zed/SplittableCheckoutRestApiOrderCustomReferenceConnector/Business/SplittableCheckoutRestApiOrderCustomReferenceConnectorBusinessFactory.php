6159193bc852f12324e50a28e8111c197a7ffaf7
gengjozsef <gengjozsef86@gmail.com>
2021-06-11 12:20:59 +0200

    implement Unit tests (#195)
    * add Unit Tests
    * code cleanup

--- bundles/splittable-checkout-rest-api-order-custom-reference-connector/src/FondOfOryx/Zed/SplittableCheckoutRestApiOrderCustomReferenceConnector/Business/SplittableCheckoutRestApiOrderCustomReferenceConnectorBusinessFactory.php
+++ bundles/splittable-checkout-rest-api-order-custom-reference-connector/src/FondOfOryx/Zed/SplittableCheckoutRestApiOrderCustomReferenceConnector/Business/SplittableCheckoutRestApiOrderCustomReferenceConnectorBusinessFactory.php
@@ -2,14 +2,14 @@
 
 namespace FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Business;
 
-use FondOfOryx\Zed\SplittableCheckoutRestApiCartNoteConnector\Business\Expander\QuoteExpander;
-use FondOfOryx\Zed\SplittableCheckoutRestApiCartNoteConnector\Business\Expander\QuoteExpanderInterface;
+use FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Business\Expander\QuoteExpander;
+use FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Business\Expander\QuoteExpanderInterface;
 use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 
 class SplittableCheckoutRestApiOrderCustomReferenceConnectorBusinessFactory extends AbstractBusinessFactory
 {
     /**
-     * @return \FondOfOryx\Zed\SplittableCheckoutRestApiCartNoteConnector\Business\Expander\QuoteExpanderInterface
+     * @return \FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Business\Expander\QuoteExpanderInterface
      */
     public function createQuoteExpander(): QuoteExpanderInterface
     {
---
+++ bundles/splittable-checkout-rest-api-order-custom-reference-connector/tests/FondOfOryx/Zed/SplittableCheckoutRestApiOrderCustomReferenceConnector/Communication/Plugin/SplittableCheckoutRestApiExtension/OrderCustomReferenceExpanderPluginTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Communication\Plugin\SplittableCheckoutRestApiExtension;
+
+use Codeception\Test\Unit;
+use FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Business\SplittableCheckoutRestApiOrderCustomReferenceConnectorFacade;
+use Generated\Shared\Transfer\QuoteTransfer;
+use Generated\Shared\Transfer\RestSplittableCheckoutRequestTransfer;
+
+class OrderCustomReferenceExpanderPluginTest extends Unit
+{
+    /**
+     * @var \FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Business\SplittableCheckoutRestApiOrderCustomReferenceConnectorFacade|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $facadeMock;
+
+    /**
+     * @var \Generated\Shared\Transfer\RestSplittableCheckoutRequestTransfer|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $restSplittableCheckoutRequestTransferMock;
+
+    /**
+     * @var \Generated\Shared\Transfer\QuoteTransfer|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected $quoteTransferMock;
+
+    /**
+     * @var \FondOfOryx\Zed\SplittableCheckoutRestApiOrderCustomReferenceConnector\Communication\Plugin\SplittableCheckoutRestApiExtension\OrderCustomReferenceQuoteExpanderPlugin
+     */
+    protected $orderCustomReferenceQuoteExpanderPlugin;
+
+    /**
+     * @return void
+     */
+    protected function _before(): void
+    {
+        parent::_before();
+
+        $this->facadeMock = $this->getMockBuilder(SplittableCheckoutRestApiOrderCustomReferenceConnectorFacade::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->restSplittableCheckoutRequestTransferMock = $this->getMockBuilder(RestSplittableCheckoutRequestTransfer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->quoteTransferMock = $this->getMockBuilder(QuoteTransfer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->orderCustomReferenceQuoteExpanderPlugin = new OrderCustomReferenceQuoteExpanderPlugin();
+        $this->orderCustomReferenceQuoteExpanderPlugin->setFacade($this->facadeMock);
+    }
+
+    /**
+     * @return void
+     */
+    public function testExpandQuote(): void
+    {
+        $this->facadeMock->expects(static::atLeastOnce())
+            ->method('expandQuote')
+            ->with(
+                $this->restSplittableCheckoutRequestTransferMock,
+                $this->quoteTransferMock
+            )->willReturn($this->quoteTransferMock);
+
+        static::assertEquals(
+            $this->quoteTransferMock,
+            $this->orderCustomReferenceQuoteExpanderPlugin->expand(
+                $this->restSplittableCheckoutRequestTransferMock,
+                $this->quoteTransferMock
+            )
+        );
+    }
+}
