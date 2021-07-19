d59cebce4fb4ab2264f8681584d2ccbe4fd3caf3
Daniel Rose <daniel-rose@gmx.de>
2021-07-19 17:25:28 +0200

    Initialize product cart code type restriction bundle (#230)
    - add contracts
    - add database schema
    - add transfer definitions
    - add initial files like composer.json, code...
    - ...

---
+++ bundles/product-cart-code-type-restriction/src/FondOfOryx/Zed/ProductCartCodeTypeRestriction/Persistence/ProductCartCodeTypeRestrictionEntityManagerInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FondOfOryx\Zed\ProductCartCodeTypeRestriction\Persistence;
+
+use Generated\Shared\Transfer\ProductAbstractCartCodeTypeRestrictionTransfer;
+
+interface ProductCartCodeTypeRestrictionEntityManagerInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\ProductAbstractCartCodeTypeRestrictionTransfer $productAbstractCartCodeTypeRestriction
+     *
+     * @return void
+     */
+    public function createProductAbstractCartCodeTypeRestriction(
+        ProductAbstractCartCodeTypeRestrictionTransfer $productAbstractCartCodeTypeRestriction
+    ): void;
+
+    /**
+     * @param int $idProductAbstract
+     * @param int[] $cartCodeTypeIds
+     *
+     * @return void
+     */
+    public function deleteProductAbstractCartCodeTypeRestrictions(
+        int $idProductAbstract,
+        array $cartCodeTypeIds
+    ): void;
+}
