c22dad412970e6f240dc89437f9436521b9d5de2
Daniel Rose <daniel-rose@gmx.de>
2021-11-17 17:39:37 +0100

    Initialize limit buyable quantity bundle
    - add initial files like composer.json, codeception.yml, ...
    - add cart precheck plugin to check added or updated item quantity (quantity <= configurable max quantity)
    - add unit tests
    ...

---
+++ bundles/limit-buyable-quantity/src/FondOfOryx/Shared/LimitBuyableQuantity/LimitBuyableQuantityConstants.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FondOfOryx\Shared\LimitBuyableQuantity;
+
+interface LimitBuyableQuantityConstants
+{
+    /**
+     * @var string
+     */
+    public const MAX_QUANTITY = 'FOND_OF_ORYX:LIMIT_BUYABLE_QUANTITY:MAX_QUANTITY';
+
+    /**
+     * @var int
+     */
+    public const MAX_QUANTITY_DEFAULT = 0;
+}
