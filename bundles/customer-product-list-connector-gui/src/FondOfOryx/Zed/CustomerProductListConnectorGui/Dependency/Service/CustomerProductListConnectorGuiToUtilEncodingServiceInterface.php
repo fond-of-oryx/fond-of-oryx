de9a1892494dde787e1beb4947765c3a6fc22f49
Daniel Rose <daniel-rose@gmx.de>
2021-12-10 13:51:07 +0100

    Initialize customer product list connector gui bundle (#378)
    - add contracts
    - add initial files like composer.json, codeception.yml, ...
    - add required transfer definitions
    - add prepare composer.json (main)
    - add template files
    - ...

---
+++ bundles/customer-product-list-connector-gui/src/FondOfOryx/Zed/CustomerProductListConnectorGui/Dependency/Service/CustomerProductListConnectorGuiToUtilEncodingServiceInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FondOfOryx\Zed\CustomerProductListConnectorGui\Dependency\Service;
+
+interface CustomerProductListConnectorGuiToUtilEncodingServiceInterface
+{
+    /**
+     * @param mixed $value
+     * @param int|null $options
+     * @param int|null $depth
+     *
+     * @return string|null
+     */
+    public function encodeJson($value, ?int $options = null, ?int $depth = null): ?string;
+
+    /**
+     * @param string $jsonValue
+     * @param bool $assoc
+     * @param int|null $depth
+     * @param int|null $options
+     *
+     * @return mixed|null
+     */
+    public function decodeJson(string $jsonValue, bool $assoc = false, ?int $depth = null, ?int $options = null);
+}
