d54d43f12a2ff8d7dc562822b9296e01f845bef8
SpiderDan98 <40319023+SpiderDan98@users.noreply.github.com>
2022-07-04 13:18:13 +0200

    ✨ Feature: add is debited condition plugin

---
+++ bundles/credit-memo-payone-debit-connector/src/FondOfOryx/Zed/CreditMemoPayoneDebitConnector/Communication/Plugin/Oms/IsDebitedConditionPlugin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FondOfOryx\Zed\CreditMemoPayoneDebitConnector\Communication\Plugin\Oms;
+
+use Orm\Zed\Sales\Persistence\SpySalesOrderItem;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+use Spryker\Zed\Oms\Dependency\Plugin\Condition\ConditionInterface;
+
+/**
+ * @method \FondOfOryx\Zed\CreditMemoPayoneDebitConnector\Business\CreditMemoPayoneDebitConnectorFacadeInterface getFacade()
+ * @method \FondOfOryx\Zed\CreditMemoPayoneDebitConnector\Business\CreditMemoPayoneDebitConnectorBusinessFactory getFactory()
+ */
+class IsDebitedConditionPlugin extends AbstractPlugin implements ConditionInterface
+{
+    /**
+     * @param \Orm\Zed\Sales\Persistence\SpySalesOrderItem $orderItem
+     *
+     * @return bool
+     */
+    public function check(SpySalesOrderItem $orderItem): bool
+    {
+        $idSalesOrderItem = $orderItem->getIdSalesOrderItem();
+        $itemTransfer = $this->getFactory()->getCreditMemoFacade()->findCreditMemoItemByIdSalesOrderItem($idSalesOrderItem);
+
+        return $itemTransfer !== null
+            && $itemTransfer->getIdCreditMemoItem() !== null
+            && $itemTransfer->getFkCreditMemo() !== null
+            && $itemTransfer->getIsDebit() === true;
+    }
+}
