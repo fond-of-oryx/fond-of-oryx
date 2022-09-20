6754bf0ae93935bff77b60235a994136d02c2a3a
Jozsef Geng <gengjozsef86@gmail.com>
2022-09-20 12:58:09 +0200

    Add implementation for the client classes

---
+++ bundles/product-list-search-rest-api/src/FondOfOryx/Client/ProductListSearchRestApi/Zed/ProductListSearchRestApiStub.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace FondOfOryx\Client\ProductListSearchRestApi\Zed;
+
+use FondOfOryx\Client\ProductListSearchRestApi\Dependency\Client\ProductListSearchRestApiToZedRequestClientInterface;
+use Generated\Shared\Transfer\ProductListCollectionTransfer;
+
+class ProductListSearchRestApiStub implements ProductListSearchRestApiStubInterface
+{
+    /**
+     * @var \FondOfOryx\Client\ProductListSearchRestApi\Dependency\Client\ProductListSearchRestApiToZedRequestClientInterface
+     */
+    protected $zedRequestClient;
+
+    /**
+     * @param \FondOfOryx\Client\ProductListSearchRestApi\Dependency\Client\ProductListSearchRestApiToZedRequestClientInterface $zedRequestClient
+     */
+    public function __construct(ProductListSearchRestApiToZedRequestClientInterface $zedRequestClient)
+    {
+        $this->zedRequestClient = $zedRequestClient;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\ProductListCollectionTransfer $productListCollectionTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductListCollectionTransfer
+     */
+    public function searchProductList(
+        ProductListCollectionTransfer $productListCollectionTransfer
+    ): ProductListCollectionTransfer {
+        /** @var \Generated\Shared\Transfer\ProductListCollectionTransfer $productListCollectionTransfer */
+        $productListCollectionTransfer = $this->zedRequestClient->call(
+            '/product-list-search-rest-api/gateway/search-product-list',
+            $productListCollectionTransfer,
+        );
+
+        return $productListCollectionTransfer;
+    }
+}
