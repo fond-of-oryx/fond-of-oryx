ae04aeb2284be78f3876185c8f6ce62a74f889e9
Julian Zimmermann <julian@jzit.de>
2022-10-14 15:58:39 +0200

    fix php cs

--- bundles/one-time-password-extension/src/FondOfOryx/Zed/OneTimePasswordExtension/Dependency/Plugin/UrlFormatterPluginInterface.php
+++ bundles/one-time-password-extension/src/FondOfOryx/Zed/OneTimePasswordExtension/Dependency/Plugin/UrlFormatterPluginInterface.php
@@ -10,6 +10,7 @@ interface UrlFormatterPluginInterface
      * @param string $path
      * @param string $encodedCredentials
      * @param \Generated\Shared\Transfer\OneTimePasswordAttributesTransfer $attributesTransfer
+     *
      * @return string
      */
     public function formatUrl(string $path, string $encodedCredentials, OneTimePasswordAttributesTransfer $attributesTransfer): string;
