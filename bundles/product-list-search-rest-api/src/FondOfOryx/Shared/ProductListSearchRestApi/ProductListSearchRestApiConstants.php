0b7e9e7237277e80b6c97a686cafd2aaba97f504
Jozsef Geng <gengjozsef86@gmail.com>
2022-10-19 09:05:38 +0200

    code cleanup

--- bundles/product-list-search-rest-api/src/FondOfOryx/Shared/ProductListSearchRestApi/ProductListSearchRestApiConstants.php
+++ bundles/product-list-search-rest-api/src/FondOfOryx/Shared/ProductListSearchRestApi/ProductListSearchRestApiConstants.php
@@ -37,7 +37,7 @@ interface ProductListSearchRestApiConstants
     /**
      * @var string
      */
-    public const FILTER_FIELD_TYPE_MAPPING = 'FOND_OF_SPRYKER:PRODUCT_LIST_SEARCH_REST_API:FILTER_FIELD_TYPE_MAPPING';
+    public const FILTER_FIELD_TYPE_MAPPING = 'FOND_OF_ORYX:PRODUCT_LIST_SEARCH_REST_API:FILTER_FIELD_TYPE_MAPPING';
 
     /**
      * @var array<string, string>
