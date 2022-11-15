dede7f70192d8aa68e50f6115474c5849337f3f4
Julian Zimmermann <54834203+julianzimmermann@users.noreply.github.com>
2022-11-15 13:15:33 +0100

    :package: adds CustomerRegistration bundle plugins (#84)

---
+++ bundles/customer-registration/src/FondOfOryx/Zed/CustomerRegistration/Communication/Plugins/PreCondition/CustomerIsNewPreConditionPlugin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FondOfOryx\Zed\CustomerRegistration\Communication\Plugins\PreCondition;
+
+use FondOfOryx\Zed\CustomerRegistrationExtension\Dependency\Plugin\CustomerRegistrationPreStepConditionPluginInterface;
+use Generated\Shared\Transfer\CustomerRegistrationRequestTransfer;
+
+class CustomerIsNewPreConditionPlugin implements CustomerRegistrationPreStepConditionPluginInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\CustomerRegistrationRequestTransfer $customerRegistrationRequestTransfer
+     *
+     * @return bool
+     */
+    public function checkPreCondition(CustomerRegistrationRequestTransfer $customerRegistrationRequestTransfer): bool
+    {
+        return $customerRegistrationRequestTransfer->getBagOrFail()->getCustomer()->getIsNew() === true;
+    }
+}
