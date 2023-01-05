1572d4f6632f2dbf9a69c0aa85286211b20d7dd5
Chris E <christopher.egger@fondof.de>
2023-01-05 14:08:29 +0100

    Chore: Inititalize checkout-rest-api-payone-connector

---
+++ bundles/checkout-rest-api-payone-connector/src/FondOfOryx/Glue/CheckoutRestApiPayoneConnector/Handler/PayoneHandlerInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FondOfOryx\Glue\CheckoutRestApiPayoneConnector\Handler;
+
+use Generated\Shared\Transfer\PaymentTransfer;
+use Generated\Shared\Transfer\RestCheckoutRequestAttributesTransfer;
+use Generated\Shared\Transfer\RestPaymentTransfer;
+
+interface PayoneHandlerInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\RestPaymentTransfer $restPaymentTransfer
+     *
+     * @return \Generated\Shared\Transfer\PaymentTransfer
+     */
+    public function preparePayment(
+        RestPaymentTransfer $restPaymentTransfer
+    ): PaymentTransfer;
+}
