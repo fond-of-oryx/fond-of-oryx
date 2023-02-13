021b696ae1c7d7b0381f858970fdf0c727947d91
Daniel Rose <daniel-rose@gmx.de>
2023-02-13 10:40:49 +0100

    ✨ Retrieve id order budget per repository call (fallback) - check database additionally for id order budget - increase minor version for "fond-of-oryx/company-business-unit-order-budget" - ...

--- bundles/company-business-unit-order-budget/src/FondOfOryx/Zed/CompanyBusinessUnitOrderBudget/Persistence/CompanyBusinessUnitOrderBudgetRepository.php
+++ bundles/company-business-unit-order-budget/src/FondOfOryx/Zed/CompanyBusinessUnitOrderBudget/Persistence/CompanyBusinessUnitOrderBudgetRepository.php
@@ -19,9 +19,24 @@ class CompanyBusinessUnitOrderBudgetRepository extends AbstractRepository implem
     public function getCompanyBusinessUnitIdsWithoutOrderBudget(): array
     {
         return $this->getFactory()->getCompanyBusinessUnitQuery()
+            ->clear()
             ->filterByFkOrderBudget(null, Criteria::ISNULL)
             ->select([SpyCompanyBusinessUnitTableMap::COL_ID_COMPANY_BUSINESS_UNIT])
             ->find()
             ->toArray();
+    }
+
+    /**
+     * @param int $idCompanyBusinessUnit
+     *
+     * @return int|null
+     */
+    public function getIdOrderBudgetByIdCompanyBusinessUnit(int $idCompanyBusinessUnit): ?int
+    {
+        return $this->getFactory()->getCompanyBusinessUnitQuery()
+            ->clear()
+            ->filterByIdCompanyBusinessUnit($idCompanyBusinessUnit)
+            ->select([SpyCompanyBusinessUnitTableMap::COL_FK_ORDER_BUDGET])
+            ->findOne();
     }
 }
