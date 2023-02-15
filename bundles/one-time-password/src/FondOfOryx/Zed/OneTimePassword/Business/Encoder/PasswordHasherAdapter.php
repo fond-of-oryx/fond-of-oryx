4ac4ffdad4bf404b4c684ca75ee8c5021ec554e1
Chris E <christopher.egger@fondof.de>
2023-02-15 11:27:33 +0100

    Add docblocks

--- bundles/one-time-password/src/FondOfOryx/Zed/OneTimePassword/Business/Encoder/PasswordHasherAdapter.php
+++ bundles/one-time-password/src/FondOfOryx/Zed/OneTimePassword/Business/Encoder/PasswordHasherAdapter.php
@@ -17,21 +17,40 @@ class PasswordHasherAdapter implements PasswordHasherInterface
      */
     protected PasswordEncoderInterface $passwordHasher;
 
+    /**
+     * @param \Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface $passwordHasher
+     */
     public function __construct(PasswordEncoderInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
     }
 
+    /**
+     * @param string $plainPassword
+     *
+     * @return string
+     */
     public function hash(string $plainPassword): string
     {
         return $this->passwordHasher->encodePassword($plainPassword, static::BCRYPT_FACTOR);
     }
 
+    /**
+     * @param string $hashedPassword
+     * @param string $plainPassword
+     *
+     * @return bool
+     */
     public function verify(string $hashedPassword, string $plainPassword): bool
     {
         return $this->passwordHasher->isPasswordValid($hashedPassword, $plainPassword, static::BCRYPT_FACTOR);
     }
 
+    /**
+     * @param string $hashedPassword
+     *
+     * @return bool
+     */
     public function needsRehash(string $hashedPassword): bool
     {
         return $this->passwordHasher->needsRehash($hashedPassword);
