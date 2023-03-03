10763fa5ee5d6224f6940f5e8c52e18bad6a9435
SpiderDan98 <40319023+SpiderDan98@users.noreply.github.com>
2023-03-03 09:15:59 +0100

    Fix: add backwords compability for username password token (#238)

--- bundles/customer-token-manager/src/FondOfOryx/Yves/CustomerTokenManager/CustomerTokenManagerFactory.php
+++ bundles/customer-token-manager/src/FondOfOryx/Yves/CustomerTokenManager/CustomerTokenManagerFactory.php
@@ -11,6 +11,7 @@ use FondOfOryx\Yves\CustomerTokenManager\Plugin\Provider\CustomerUserProvider;
 use FondOfOryx\Yves\CustomerTokenManager\Plugin\Security\CustomerTokenManagerSecurityPlugin;
 use FondOfOryx\Yves\CustomerTokenManager\Security\Customer;
 use Generated\Shared\Transfer\CustomerTransfer;
+use ReflectionClass;
 use Spryker\Yves\Kernel\AbstractFactory;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -40,6 +41,15 @@ class CustomerTokenManagerFactory extends AbstractFactory
     public function createUsernamePasswordToken(CustomerTransfer $customerTransfer): TokenInterface
     {
         $user = $this->createSecurityUser($customerTransfer);
+
+        if (count((new ReflectionClass(UsernamePasswordToken::class))->getConstructor()->getParameters()) === 4) {
+            return new UsernamePasswordToken(
+                $user,
+                $user->getPassword(),
+                CustomerTokenManagerConfig::SECURITY_FIREWALL_NAME,
+                [CustomerTokenManagerSecurityPlugin::ROLE_USER],
+            );
+        }
 
         return new UsernamePasswordToken(
             $user,
