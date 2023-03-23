5dce62f99c4930f452b4bdcf9c8e45e35591c0a9
Jozsef Geng <gengjozsef86@gmail.com>
2023-03-23 10:57:56 +0100

    ✨Handle query parameters is-canceled, from & to for erp-orders-page-search-request - add plugins to expand search query with is-canceled, from & to - add isCanceled property to transfer object - add constants - prepare and add unit tests - ...

---
+++ bundles/erp-order-page-search/src/FondOfOryx/Client/ErpOrderPageSearch/Plugin/SearchExtension/FromDateErpOrderPageSearchQueryExpanderPlugin.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace FondOfOryx\Client\ErpOrderPageSearch\Plugin\SearchExtension;
+
+use DateTime;
+use Elastica\Query;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\Range;
+use FondOfOryx\Shared\ErpOrderPageSearch\ErpOrderPageSearchConstants;
+use FondOfSpryker\Shared\ConditionalAvailabilityPageSearch\ConditionalAvailabilityPageSearchConstants;
+use Generated\Shared\Search\ConditionalAvailabilityPeriodIndexMap;
+use Generated\Shared\Search\ErpOrderIndexMap;
+use Generated\Shared\Search\PageIndexMap;
+use InvalidArgumentException;
+use Spryker\Client\Kernel\AbstractPlugin;
+use Spryker\Client\SearchExtension\Dependency\Plugin\QueryExpanderPluginInterface;
+use Spryker\Client\SearchExtension\Dependency\Plugin\QueryInterface;
+
+class FromDateErpOrderPageSearchQueryExpanderPlugin extends AbstractPlugin implements QueryExpanderPluginInterface {
+    /**
+     * @param \Spryker\Client\SearchExtension\Dependency\Plugin\QueryInterface $searchQuery
+     * @param array $requestParameters
+     *
+     * @return \Spryker\Client\SearchExtension\Dependency\Plugin\QueryInterface
+     */
+    public function expandQuery(QueryInterface $searchQuery, array $requestParameters = [])
+    {
+        if (!isset($requestParameters[ErpOrderPageSearchConstants::PARAMETER_FROM])) {
+            return $searchQuery;
+        }
+
+        $from = new DateTime($requestParameters[ErpOrderPageSearchConstants::PARAMETER_FROM]);
+        $boolQuery = $this->getBoolQuery($searchQuery->getSearchQuery());
+
+        $fromRange = (new Range())->addField(
+            ErpOrderIndexMap::CREATED_AT,
+            [
+                'gte' => $from->format('Y-m-d H:i:s'),
+            ],
+        );
+
+        $boolQuery->addFilter($fromRange);
+
+        return $searchQuery;
+    }
+
+    /**
+     * @param \Elastica\Query $query
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return \Elastica\Query\BoolQuery
+     */
+    protected function getBoolQuery(Query $query): BoolQuery
+    {
+        $boolQuery = $query->getQuery();
+
+        if (!$boolQuery instanceof BoolQuery) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Query expander available only with %s, got: %s',
+                    BoolQuery::class,
+                    get_class($boolQuery),
+                ),
+            );
+        }
+
+        return $boolQuery;
+    }
+}
