cdf2b172ac20be20e6c28dbb33a949a0e26615e5
Daniel Rose <daniel-rose@gmx.de>
2023-03-29 09:31:23 +0200

    ✨ Implement glue contracts to business-on-behalf-rest-api-bundle - allow to set default company user per glue endpoint business-on-behalf - there is only one default company user per customer - add unit tests - ...

---
+++ bundles/business-on-behalf-rest-api/src/FondOfOryx/Glue/BusinessOnBehalfRestApi/Processor/Builder/RestResponseBuilder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace FondOfOryx\Glue\BusinessOnBehalfRestApi\Processor\Builder;
+
+use FondOfOryx\Shared\BusinessOnBehalfRestApi\BusinessOnBehalfRestApiConstants;
+use Generated\Shared\Transfer\RestErrorMessageTransfer;
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface;
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class RestResponseBuilder implements RestResponseBuilderInterface
+{
+    /**
+     * @var \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
+     */
+    protected RestResourceBuilderInterface $restResourceBuilder;
+
+    /**
+     * @param \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface $restResourceBuilder
+     */
+    public function __construct(
+        RestResourceBuilderInterface $restResourceBuilder
+    ) {
+        $this->restResourceBuilder = $restResourceBuilder;
+    }
+
+    /**
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface
+     */
+    public function buildEmptyRestResponse(): RestResponseInterface
+    {
+        return $this->restResourceBuilder->createRestResponse()
+            ->setStatus(Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @param array<\Generated\Shared\Transfer\RestBusinessOnBehalfErrorTransfer> $restBusinessOnBehalfErrorTransfers
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface
+     */
+    public function buildErrorRestResponse(array $restBusinessOnBehalfErrorTransfers): RestResponseInterface
+    {
+        $restResponse = $this->restResourceBuilder
+            ->createRestResponse();
+
+        if (count($restBusinessOnBehalfErrorTransfers) === 0) {
+            $restErrorMessageTransfer = (new RestErrorMessageTransfer())
+                ->setCode(BusinessOnBehalfRestApiConstants::ERROR_CODE_UNDEFINED_ERROR_OCCURRED)
+                ->setStatus(Response::HTTP_BAD_REQUEST)
+                ->setDetail(BusinessOnBehalfRestApiConstants::ERROR_MESSAGE_UNDEFINED_ERROR_OCCURRED);
+
+            $restResponse->addError($restErrorMessageTransfer);
+        }
+
+        foreach ($restBusinessOnBehalfErrorTransfers as $restBusinessOnBehalfErrorTransfer) {
+            $restErrorMessageTransfer = (new RestErrorMessageTransfer())
+                ->setCode($restBusinessOnBehalfErrorTransfer->getErrorCode())
+                ->setStatus(Response::HTTP_BAD_REQUEST)
+                ->setDetail($restBusinessOnBehalfErrorTransfer->getMessage());
+
+            $restResponse->addError($restErrorMessageTransfer);
+        }
+
+        return $restResponse;
+    }
+}
