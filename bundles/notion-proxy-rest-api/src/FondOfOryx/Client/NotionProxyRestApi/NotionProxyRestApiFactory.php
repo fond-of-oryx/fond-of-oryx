80e8f64b7e115a6b859cd4be2b0d56bab4f3b347
Jozsef Geng <jozsef.geng@fondof.de>
2023-05-22 12:06:11 +0200

    code refractor: instantiate in the BusinessFactory

--- bundles/notion-proxy-rest-api/src/FondOfOryx/Client/NotionProxyRestApi/NotionProxyRestApiFactory.php
+++ bundles/notion-proxy-rest-api/src/FondOfOryx/Client/NotionProxyRestApi/NotionProxyRestApiFactory.php
@@ -4,8 +4,6 @@ namespace FondOfOryx\Client\NotionProxyRestApi;
 
 use FondOfOryx\Client\NotionProxyRestApi\Request\Request;
 use FondOfOryx\Client\NotionProxyRestApi\Request\RequestInterface;
-use FondOfOryx\Shared\NotionProxyRestApi\GuzzleHttpClient\GuzzleHttpClientFactory;
-use FondOfOryx\Shared\NotionProxyRestApi\GuzzleHttpClient\GuzzleHttpClientFactoryInterface;
 use GuzzleHttp\Client;
 use Spryker\Client\Kernel\AbstractFactory;
 
@@ -19,24 +17,16 @@ class NotionProxyRestApiFactory extends AbstractFactory
      */
     public function createRequestClient(): RequestInterface
     {
-        return new Request($this->getGuzzleHttpClient());
+        return new Request($this->createGuzzleClient());
     }
 
     /**
      * @return \GuzzleHttp\Client
      */
-    protected function getGuzzleHttpClient(): Client
+    protected function createGuzzleClient(): Client
     {
-        return $this->createGuzzleHttpClientFactory()->createClient(
-            $this->getConfig()->getClientConfig(),
+        return new Client(
+            $this->getConfig()->getClientConfig()
         );
-    }
-
-    /**
-     * @return \FondOfOryx\Shared\NotionProxyRestApi\GuzzleHttpClient\GuzzleHttpClientFactoryInterface
-     */
-    protected function createGuzzleHttpClientFactory(): GuzzleHttpClientFactoryInterface
-    {
-        return new GuzzleHttpClientFactory();
     }
 }
