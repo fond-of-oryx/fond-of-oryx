4e145ac1a50038860ae054f223960e6d35828fb3
Daniel Rose <daniel-rose@gmx.de>
2023-05-23 14:17:55 +0200

    ✨ Add `company-user-search-rest-api-extension`-bundle - provide plugin to expand list of filter fields (glue layer) - provide plugin to expand company user search query (zed layer) - github action for split repo - ...

---
+++ bundles/company-user-search-rest-api-extension/src/FondOfOryx/Zed/CompanyUserSearchRestApiExtension/Dependency/Plugin/SearchCompanyUserQueryExpanderPluginInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+use Generated\Shared\Transfer\QueryJoinCollectionTransfer;
+
+interface SearchCompanyUserQueryExpanderPluginInterface
+{
+    /**
+     * Specification:
+     * - Returns true if plugin is applicable for given filters.
+     *
+     * @api
+     *
+     * @param array<\Generated\Shared\Transfer\FilterFieldTransfer> $filterFieldTransfers
+     *
+     * @return bool
+     */
+    public function isApplicable(array $filterFieldTransfers): bool;
+
+    /**
+     * Specification:
+     * - Expands QueryJoinCollectionTransfer with additional QueryJoinTransfers.
+     *
+     * @api
+     *
+     * @param array<\Generated\Shared\Transfer\FilterFieldTransfer> $filterFieldTransfers
+     * @param \Generated\Shared\Transfer\QueryJoinCollectionTransfer $queryJoinCollectionTransfer
+     *
+     * @return \Generated\Shared\Transfer\QueryJoinCollectionTransfer
+     */
+    public function expand(
+        array $filterFieldTransfers,
+        QueryJoinCollectionTransfer $queryJoinCollectionTransfer
+    ): QueryJoinCollectionTransfer;
+}
