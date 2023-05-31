46d58f993a0c52c467050089c93d1ef49323055f
Daniel Rose <daniel-rose@gmx.de>
2023-05-31 17:54:50 +0200

    ✨ Add `company-search-rest-api-extension`-bundle - provide plugin to expand list of filter fields (glue layer) - provide plugin to expand company search query (zed layer) - github action for split repo - ...

---
+++ bundles/company-search-rest-api-extension/src/FondOfOryx/Glue/CompanySearchRestApiExtension/Dependency/Plugin/FilterFieldsExpanderPluginInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FondOfOryx\Glue\CompanySearchRestApiExtension\Dependency\Plugin;
+
+use ArrayObject;
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+
+interface FilterFieldsExpanderPluginInterface
+{
+    /**
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     * @param \ArrayObject<\Generated\Shared\Transfer\FilterFieldTransfer> $filterFieldTransfers
+     *
+     * @return \ArrayObject<\Generated\Shared\Transfer\FilterFieldTransfer>
+     */
+    public function expand(
+        RestRequestInterface $restRequest,
+        ArrayObject $filterFieldTransfers
+    ): ArrayObject;
+}
