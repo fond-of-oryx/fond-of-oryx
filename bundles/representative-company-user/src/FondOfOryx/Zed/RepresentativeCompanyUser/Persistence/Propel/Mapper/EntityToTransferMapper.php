f71af64e622aef2eadd6e6f0217eb86a1dc92087
Julian Hyatt <dev@jzit.de>
2023-06-05 16:53:54 +0200

    rename status in transfer to state fix mapping on null entity

--- bundles/representative-company-user/src/FondOfOryx/Zed/RepresentativeCompanyUser/Persistence/Propel/Mapper/EntityToTransferMapper.php
+++ bundles/representative-company-user/src/FondOfOryx/Zed/RepresentativeCompanyUser/Persistence/Propel/Mapper/EntityToTransferMapper.php
@@ -24,12 +24,14 @@ class EntityToTransferMapper implements EntityToTransferMapperInterface
     }
 
     /**
-     * @param \Orm\Zed\Customer\Persistence\SpyCustomer $customerEntity
-     *
-     * @return \Generated\Shared\Transfer\CustomerTransfer
+     * @param SpyCustomer|null $customerEntity
+     * @return CustomerTransfer|null
      */
-    protected function mapCustomer(SpyCustomer $customerEntity): CustomerTransfer
+    protected function mapCustomer(?SpyCustomer $customerEntity): ?CustomerTransfer
     {
+        if ($customerEntity === null){
+            return null;
+        }
         return (new CustomerTransfer())->fromArray($customerEntity->toArray(), true);
     }
 }
