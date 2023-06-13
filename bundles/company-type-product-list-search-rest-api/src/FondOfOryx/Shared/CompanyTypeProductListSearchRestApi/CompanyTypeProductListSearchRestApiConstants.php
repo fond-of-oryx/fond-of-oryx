a58074036d63ef43f804a711096d6df14445951a
Daniel Rose <daniel-rose@gmx.de>
2023-06-13 10:25:42 +0200

    handle request parameter `customer-id`

---
+++ bundles/company-type-product-list-search-rest-api/src/FondOfOryx/Shared/CompanyTypeProductListSearchRestApi/CompanyTypeProductListSearchRestApiConstants.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace FondOfOryx\Shared\CompanyTypeProductListSearchRestApi;
+
+interface CompanyTypeProductListSearchRestApiConstants
+{
+    /**
+     * @var string
+     */
+    public const FILTER_FIELD_TYPE_FOREIGN_CUSTOMER_REFERENCE = 'foreignCustomerReference';
+
+    /**
+     * @var string
+     */
+    public const FILTER_FIELD_TYPE_ID_CUSTOMER = 'idCustomer';
+
+    /**
+     * @var string
+     */
+    public const PARAMETER_NAME_CUSTOMER_ID = 'customer-id';
+}
