23dad289c8f044fc8affa6add63c6e4d819adf03
Daniel Rose <daniel-rose@gmx.de>
2023-09-26 15:29:48 +0200

    ✨ Add `business-on-behalf-company-user-search-rest-api`-bundle (#442)
    - handle request parameter `is-default`
    - add entry to dandelion.json
    - ...

---
+++ bundles/business-on-behalf-company-user-search-rest-api/src/FondOfOryx/Shared/BusinessOnBehalfCompanyUserSearchRestApi/BusinessOnBehalfCompanyUserSearchRestApiConstants.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FondOfOryx\Shared\BusinessOnBehalfCompanyUserSearchRestApi;
+
+interface BusinessOnBehalfCompanyUserSearchRestApiConstants
+{
+    /**
+     * @var string
+     */
+    public const PARAMETER_NAME_IS_DEFAULT = 'is-default';
+
+    /**
+     * @var string
+     */
+    public const FILTER_FIELD_TYPE_IS_DEFAULT = 'is-default';
+}
