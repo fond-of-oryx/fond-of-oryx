290b4cf188bd7c1cc5798e83f12b9dc142318a14
Daniel Rose <daniel-rose@gmx.de>
2023-09-27 17:27:24 +0200

    ✨ Add `company-user-cart-search-rest-api`-bundle - handle request parameter company-user-reference - add entry to dandelion.json - ...

---
+++ bundles/company-user-cart-search-rest-api/tests/FondOfOryx/Zed/CompanyUserCartSearchRestApi/Communication/Plugin/CartSearchRestApiExtension/CompanyUserSearchQuoteQueryExpanderPluginTest.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace FondOfOryx\Zed\CompanyUserCartSearchRestApi\Communication\Plugin\CartSearchRestApiExtension;
+
+use Codeception\Test\Unit;
+use FondOfOryx\Shared\CompanyUserCartSearchRestApi\CompanyUserCartSearchRestApiConstants;
+use Generated\Shared\Transfer\FilterFieldTransfer;
+use Generated\Shared\Transfer\QueryJoinCollectionTransfer;
+use Generated\Shared\Transfer\QueryJoinTransfer;
+use Orm\Zed\Quote\Persistence\Map\SpyQuoteTableMap;
+use PHPUnit\Framework\MockObject\MockObject;
+use Propel\Runtime\ActiveQuery\Criteria;
+
+class CompanyUserSearchQuoteQueryExpanderPluginTest extends Unit
+{
+    /**
+     * @var array<(\Generated\Shared\Transfer\FilterFieldTransfer&\PHPUnit\Framework\MockObject\MockObject)|\PHPUnit\Framework\MockObject\MockObject>
+     */
+    protected array $filterFieldTransferMocks;
+
+    /**
+     * @var (\Generated\Shared\Transfer\QueryJoinCollectionTransfer&\PHPUnit\Framework\MockObject\MockObject)|\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected MockObject|QueryJoinCollectionTransfer $queryJoinCollectionTransferMock;
+
+    /**
+     * @var \FondOfOryx\Zed\CompanyUserCartSearchRestApi\Communication\Plugin\CartSearchRestApiExtension\CompanyUserSearchQuoteQueryExpanderPlugin
+     */
+    protected CompanyUserSearchQuoteQueryExpanderPlugin $plugin;
+
+    /**
+     * @return void
+     */
+    protected function _before(): void
+    {
+        parent::_before();
+
+        $this->filterFieldTransferMocks = [
+            $this->getMockBuilder(FilterFieldTransfer::class)
+                ->disableOriginalConstructor()
+                ->getMock(),
+            $this->getMockBuilder(FilterFieldTransfer::class)
+                ->disableOriginalConstructor()
+                ->getMock(),
+        ];
+
+        $this->queryJoinCollectionTransferMock = $this->getMockBuilder(QueryJoinCollectionTransfer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->plugin = new CompanyUserSearchQuoteQueryExpanderPlugin();
+    }
+
+    /**
+     * @return void
+     */
+    public function testIsApplicable(): void
+    {
+        $this->filterFieldTransferMocks[0]->expects(static::atLeastOnce())
+            ->method('getType')
+            ->willReturn('foo');
+
+        $this->filterFieldTransferMocks[1]->expects(static::atLeastOnce())
+            ->method('getType')
+            ->willReturn(CompanyUserCartSearchRestApiConstants::FILTER_FIELD_TYPE_COMPANY_USER_REFERENCE);
+
+        static::assertTrue($this->plugin->isApplicable($this->filterFieldTransferMocks));
+    }
+
+    /**
+     * @return void
+     */
+    public function testIsApplicableWithInvalidFilterFieldTypes(): void
+    {
+        $this->filterFieldTransferMocks[0]->expects(static::atLeastOnce())
+            ->method('getType')
+            ->willReturn('foo');
+
+        $this->filterFieldTransferMocks[1]->expects(static::atLeastOnce())
+            ->method('getType')
+            ->willReturn('bar');
+
+        static::assertFalse($this->plugin->isApplicable($this->filterFieldTransferMocks));
+    }
+
+    /**
+     * @return void
+     */
+    public function testExpand(): void
+    {
+        $companyUserReference = 'foo';
+
+        $this->filterFieldTransferMocks[0]->expects(static::atLeastOnce())
+            ->method('getType')
+            ->willReturn('foo');
+
+        $this->filterFieldTransferMocks[1]->expects(static::atLeastOnce())
+            ->method('getType')
+            ->willReturn(CompanyUserCartSearchRestApiConstants::FILTER_FIELD_TYPE_COMPANY_USER_REFERENCE);
+
+        $this->filterFieldTransferMocks[1]->expects(static::atLeastOnce())
+            ->method('getValue')
+            ->willReturn($companyUserReference);
+
+        $this->queryJoinCollectionTransferMock->expects(static::atLeastOnce())
+            ->method('addQueryJoin')
+            ->with(
+                static::callback(
+                    fn (
+                        QueryJoinTransfer $queryJoinTransfer
+                    ) => $queryJoinTransfer->getWhereConditions()->count() === 1
+                        && $queryJoinTransfer->getWhereConditions()->offsetGet(0)->getColumn() === SpyQuoteTableMap::COL_COMPANY_USER_REFERENCE
+                        && $queryJoinTransfer->getWhereConditions()->offsetGet(0)->getComparison() === Criteria::EQUAL
+                        && $queryJoinTransfer->getWhereConditions()->offsetGet(0)->getValue() === $companyUserReference
+                ),
+            )->willReturn($this->queryJoinCollectionTransferMock);
+
+        static::assertEquals(
+            $this->queryJoinCollectionTransferMock,
+            $this->plugin->expand(
+                $this->filterFieldTransferMocks,
+                $this->queryJoinCollectionTransferMock,
+            ),
+        );
+    }
+}
