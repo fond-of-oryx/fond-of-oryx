b96eff3ad14ee4e20c638b994089fa34b744d302
Julian Hyatt <dev@jzit.de>
2023-10-24 16:25:34 +0200

    ✨ introduce new interface

---
+++ bundles/company-users-rest-api-extension/src/FondOfOryx/Zed/CompanyUsersRestApiExtension/Dependency/Plugin/QueryExpander/CompanyUserQueryExpanderPluginInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FondOfOryx\Zed\CompanyUsersRestApiExtension\Dependency\Plugin\QueryExpander;
+
+use Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery;
+
+interface CompanyUserQueryExpanderPluginInterface
+{
+    /**
+     * @param \Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery $companyUserQuery
+     * @return \Orm\Zed\CompanyUser\Persistence\SpyCompanyUserQuery
+     */
+    public function expandCompanyUserQuery(SpyCompanyUserQuery $companyUserQuery): SpyCompanyUserQuery;
+}
