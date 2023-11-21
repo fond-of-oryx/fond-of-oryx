1858b5e54602af9e3aae230030ee0b5dde526c30
Julian Hyatt <54834203+julianzimmermann@users.noreply.github.com>
2023-11-21 10:02:48 +0100

    ✨ migrate make file generate-composer-sections (#416)
    Co-authored-by: Daniel Rose <daniel-rose@gmx.de>

--- Makefile.d/composer.sh
+++ Makefile.d/composer.sh
@@ -59,7 +59,79 @@ function generate_sections {
     generate_autoload_dev_section
 }
 
+function generate_replace_section {
+    REPLACE=()
+    for i in $(ls bundles/)
+    do
+        REPLACE+=("\"fond-of-oryx/$i\": \"*\"")
+    done
+
+    REPLACE_AS_STRING=$(IFS=, ; echo "{${REPLACE[*]}}")
+    mv composer.json composer.json.old
+    cat composer.json.old | Makefile.d/jq -r --argjson replace "$REPLACE_AS_STRING" '.replace |= $replace' > composer.json
+    rm composer.json.old
+}
+
+function generate_autoload_section {
+    AUTOLOAD=()
+    for i in $(ls bundles/)
+    do
+        for j in $(ls bundles/$i/src/FondOfOryx/)
+        do
+            for k in $(ls bundles/$i/src/FondOfOryx/$j)
+            do
+                AUTOLOAD+=("\"FondOfOryx\\\\$j\\\\$k\\\\\": \"bundles/$i/src/FondOfOryx/$j/$k\"")
+            done
+        done
+    done
+
+    AUTOLOAD_AS_STRING=$(IFS=, ; echo "{${AUTOLOAD[*]}}")
+    mv composer.json composer.json.old
+    cat composer.json.old | Makefile.d/jq -r --argjson autoload "$AUTOLOAD_AS_STRING" '.autoload."psr-4" |= $autoload' > composer.json
+    rm composer.json.old
+}
+
+function generate_autoload_dev_section {
+    AUTOLOAD_DEV=()
+    for i in $(ls bundles/)
+    do
+        for j in $(ls bundles/$i/src/FondOfOryx/)
+        do
+            for k in $(ls bundles/$i/src/FondOfOryx/$j)
+            do
+                AUTOLOAD_DEV+=("\"FondOfOryx\\\\$j\\\\$k\\\\\": \"bundles/$i/tests/FondOfOryx/$j/$k\"")
+            done
+        done
+    done
+
+    AUTOLOAD_DEV+=("\"Generated\\\\\": \"src/Generated/\"")
+    AUTOLOAD_DEV+=("\"Orm\\\\Zed\\\\\": \"src/Orm/Zed/\"")
+
+    AUTOLOAD_DEV_AS_STRING=$(IFS=, ; echo "{${AUTOLOAD_DEV[*]}}")
+    mv composer.json composer.json.old
+    cat composer.json.old | Makefile.d/jq -r --argjson autoloadDev "$AUTOLOAD_DEV_AS_STRING" '."autoload-dev"."psr-4" |= $autoloadDev' > composer.json
+    rm composer.json.old
+}
+
+function generate_sections {
+    generate_replace_section
+    generate_autoload_section
+    generate_autoload_dev_section
+}
+
 case "$1" in
+    generate_replace_section)
+        generate_replace_section
+        ;;
+    generate_autoload_section)
+        generate_autoload_section
+        ;;
+    generate_autoload_dev_section)
+        generate_autoload_dev_section
+        ;;
+    generate_sections)
+        generate_sections
+        ;;
     generate_replace_section)
         generate_replace_section
         ;;
