58262f42074a4aaf4b39d32b2e2e4c50b8fe7ffe
gengjozsef <gengjozsef86@gmail.com>
2024-01-15 14:23:22 +0100

    ✨ implement `lowercase behavior`

---
+++ bundles/lowercase-behavior/src/FondOfOryx/Zed/LowercaseBehavior/Persistence/Propel/Behavior/Exception/ColumnNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FondOfOryx\Zed\LowercaseBehavior\Persistence\Propel\Behavior\Exception;
+
+use Propel\Runtime\Exception\PropelException;
+
+class ColumnNotFoundException extends PropelException
+{
+}
