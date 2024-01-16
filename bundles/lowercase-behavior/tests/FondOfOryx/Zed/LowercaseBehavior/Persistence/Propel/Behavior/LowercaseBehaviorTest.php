812c3ca8b21023d7918cacc42cbeb582aaab7088
gengjozsef <gengjozsef86@gmail.com>
2024-01-16 12:08:59 +0100

    ✨ release lowercase-behavior (#504)
    * ✨ release `lowercase-behavior`
    
    * ✨ add initial implementation for `Unit Test`
    
    * 🐛 fix phpcs

---
+++ bundles/lowercase-behavior/tests/FondOfOryx/Zed/LowercaseBehavior/Persistence/Propel/Behavior/LowercaseBehaviorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace FondOfOryx\Zed\LowercaseBehavior\Persistence\Propel\Behavior;
+
+use Codeception\Test\Unit;
+use Propel\Generator\Util\QuickBuilder as PropelQuickBuilder;
+
+class LowercaseBehaviorTest extends Unit
+{
+    /**
+     * @return void
+     */
+   /*public function testGenerateColumnValuesToLowercaseByBody(): void
+    {
+        // Arrange
+        $this->buildPropelEntities();
+
+        // Act
+        $testEntity = new TestLowercaseBehavior();
+        $testEntity->setEmail('Max@Mustermann.de');
+        $testEntity->save();
+
+        // Assert
+        $this->assertSame($testEntity->getEmail(), "max@mustermann.de");
+    }*/
+
+    /**
+     * @uses TestLowercaseBehavior
+     *
+     * @return void
+     */
+    protected function buildPropelEntities(): void
+    {
+        if (class_exists('TestLowercaseBehavior')) {
+            return;
+        }
+
+        $schema = '
+            <database name="test_lowercase_behavior" defaultIdMethod="native">
+                <table name="test_lowercase_behavior">
+                    <column name="email" required="true" size="255" type="VARCHAR"/>
+                    <behavior name="lowercase">
+                        <parameter name="lowercase_columns" value="email"/>
+                    </behavior>
+                </table>
+            </database>';
+
+        PropelQuickBuilder::buildSchema($schema);
+    }
+}
