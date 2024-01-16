dbac7ff63b1961c1d5eb2b6308e7735a7c515e86
gengjozsef <gengjozsef86@gmail.com>
2024-01-16 10:32:53 +0100

    ✨ implement `lowercase behavior` (#503)
    * ✨ implement `lowercase behavior`
    
    * 🐛 fix phpcs erors
    
    * 🐛 fix `Copyright` year information

---
+++ bundles/lowercase-behavior/README.md
@@ -0,0 +1,10 @@
+# LowercaseBehavior Module
+
+[![CI](https://github.com/fond-of-oryx/lowercase-behavior/actions/workflows/main.yml/badge.svg)](https://github.com/fond-of-oryx/lowercase-behavior/actions/workflows/main.yml)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://packagist.org/packages/fond-of-oryx/lowercase-behavior)
+
+## Installation
+
+```
+composer require fond-of-oryx/lowercase-behavior
+```
---
+++ bundles/lowercase-behavior/src/FondOfOryx/Zed/LowercaseBehavior/Persistence/Propel/Behavior/LowercaseBehavior.php
@@ -0,0 +1,93 @@
+<?php
+namespace FondOfOryx\Zed\LowercaseBehavior\Persistence\Propel\Behavior;
+
+use Propel\Generator\Model\Behavior;
+use Spryker\Zed\UuidBehavior\Persistence\Propel\Behavior\Exception\ColumnNotFoundException;
+
+class LowercaseBehavior extends Behavior
+{
+    /**
+     * @var string
+     */
+    protected const KEY_PARAMETER_LOWERCASE_COLUMN = 'lowercase_columns';
+
+    /**
+     * @var string
+     */
+    protected const ERROR_COLUMN_NOT_FOUND = 'Column %s does not exist in table %s.';
+
+    /**
+     * @return string
+     */
+    public function objectMethods(): string
+    {
+        return $this->addColumnValuesToLowercase();
+    }
+
+    /**
+     * @return string
+     */
+    public function preSave(): string
+    {
+        return "
+        \$this->columnValuesToLowercase();
+        ";
+    }
+
+    /**
+     * @throws \Spryker\Zed\UuidBehavior\Persistence\Propel\Behavior\Exception\ColumnNotFoundException
+     *
+     * @return string
+     */
+    protected function addColumnValuesToLowercase(): string
+    {
+        $body = '';
+        $parameters = $this->getParameters();
+
+        if (!isset($parameters[static::KEY_PARAMETER_LOWERCASE_COLUMN])) {
+            return $this->generateColumnValuesToLowercaseByBody($body);
+        }
+
+        $lowercaseColumns = explode(',', $parameters[static::KEY_PARAMETER_LOWERCASE_COLUMN]);
+
+        foreach ($lowercaseColumns as $index => $lowercaseColumn) {
+            if (!$this->getTable()->hasColumn($lowercaseColumn)) {
+                throw new ColumnNotFoundException(sprintf(
+                    static::ERROR_COLUMN_NOT_FOUND,
+                    $lowercaseColumn,
+                    $this->getTable()->getName(),
+                ));
+            }
+
+            $pascalCaseColumn = str_replace(' ', '', ucwords(str_replace('_', ' ', $lowercaseColumn)));
+            $getterMethodName = sprintf('get%s', $pascalCaseColumn);
+            $setterMethodName = sprintf('set%s', $pascalCaseColumn);
+
+            if ($index > 0) {
+                $body .= '';
+            }
+
+            $body .= "\$this->$setterMethodName(strtolower(\$this->$getterMethodName()));";
+        }
+
+        return $this->generateColumnValuesToLowercaseByBody($body);
+    }
+
+    /**
+     * @param string $body
+     *
+     * @return string
+     */
+    protected function generateColumnValuesToLowercaseByBody(string $body = ''): string
+    {
+        return "
+/**
+ * @return void
+ */
+protected function columnValuesToLowercase()
+{
+    $body
+}
+        ";
+    }
+}
